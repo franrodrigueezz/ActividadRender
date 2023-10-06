ed45b1ac93de0aaacd58b12aba96d37218d439cf
franrodrigueezz <frodriguez@itpfp.com>
2023-10-06 13:28:52 +0200

    Segundo commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile(__dirname, 'Index.html')
 })
 
 app.listen(3000)
